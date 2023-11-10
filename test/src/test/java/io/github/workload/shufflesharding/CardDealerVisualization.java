159fe90dd50e3d45e5f0bf32d38753715fc051eb
gaopeng71 <gaopeng8@jd.com>
2023-11-10 14:13:11 +0800

    用颜色提升可视化效果

--- test/src/test/java/io/github/workload/shufflesharding/CardDealerVisualization.java
+++ test/src/test/java/io/github/workload/shufflesharding/CardDealerVisualization.java
@@ -12,8 +12,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 class CardDealerVisualization {
-    private static final String NEWLINE = System.getProperty("line.separator");
-
     private int nodes = 10;
     private int handSize = 2;
     private int dbShards = 4;
@@ -38,11 +36,14 @@ class CardDealerVisualization {
         for (int node : node2user.keySet()) {
             List<Integer> uidList = node2user.get(node);
             for (int uid : uidList) {
-                d2.append(String.format("Node%d -> U%d", node, uid)).append(NEWLINE);
+                addLine(String.format("Node%d -> U%d {", node, uid));
+                addLine(String.format("  style.stroke: %s", colors[node % colors.length]));
+                addLine(" style.stroke-width: 5");
+                addLine("}");
             }
         }
         for (int uid = 0; uid < users; uid++) {
-            d2.append(String.format("U%d -> DB%d", uid, uid % dbShards)).append(NEWLINE);
+            addLine(String.format("U%d -> DB%d", uid, uid % dbShards));
         }
         dumpToFile("../doc/shufflesharding.d2", d2.toString());
     }
@@ -52,5 +53,45 @@ class CardDealerVisualization {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.append(content);
         }
+    }
+
+
+    private static String[] colors = new String[]{
+            "aqua",
+            "aquamarine",
+            "black",
+            "blue",
+            "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse",
+            "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson",
+            "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray",
+            "darkgreen", "darkgrey", "darkkhaki", "darkmagenta", "darkolivegreen",
+            "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen",
+            "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet",
+            "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue",
+            "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro",
+            "ghostwhite", "gold", "goldenrod", "gray", "green",
+            "greenyellow", "grey", "honeydew", "hotpink", "indianred",
+            "indigo", "ivory", "khaki", "lavender", "lavenderblush",
+            "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan",
+            "lightgoldenrodyellow", "lightgray", "lightgreen", "lightgrey", "lightpink",
+            "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey",
+            "lightsteelblue", "lightyellow", "lime", "limegreen", "linen",
+            "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid",
+            "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise",
+            "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin",
+            "navajowhite", "navy", "oldlace", "olive", "olivedrab",
+            "orange", "orangered", "orchid", "palegoldenrod", "palegreen",
+            "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru",
+            "pink", "plum", "powderblue", "purple", "rebeccapurple",
+            "red", "rosybrown", "royalblue", "saddlebrown", "salmon",
+            "sandybrown", "seagreen", "seashell", "sienna", "silver",
+            "skyblue", "slateblue", "slategray", "slategrey", "snow",
+            "springgreen", "steelblue", "tan", "teal", "thistle",
+            "tomato", "turquoise", "violet", "wheat", "white",
+            "whitesmoke", "yellow", "yellowgreen"
+    };
+
+    private void addLine(String content) {
+        d2.append(content).append(System.getProperty("line.separator"));
     }
 }
