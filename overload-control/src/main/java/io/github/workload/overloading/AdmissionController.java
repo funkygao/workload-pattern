444e6b24d356dbfbd3f20f8a4a0d8601df484f2e
gaopeng71 <gaopeng8@jd.com>
2023-12-07 15:04:33 +0800

    the abstration of admission controller

--- overload-control/src/main/java/io/github/workload/overloading/AdmissionController.java
+++ overload-control/src/main/java/io/github/workload/overloading/AdmissionController.java
@@ -1,120 +1,43 @@
 package io.github.workload.overloading;
 
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.RejectedExecutionHandler;
 
 /**
- * 面向QoS的自适应式工作负载准入管制，阀门，可用于RPC/异步任务排队/MQ消费等场景.
+ * 基于反馈控制原理的自适应式工作负载准入控制器.
  *
- * <pre>
- *                         AdmissionController
- *                   IN  +───────────────────────+  OUT
- * WorkloadPriority ───> │ AdmissionLevel        │ ─────> deny/accept
- *                       +───────────────────────+
- *                       │ +admit(P)             │
- *                       │ +overloaded()         │
- *                       │ +recordQueuedNs(ns)   │
- *                       +───────────────────────+
- *                        window: (time, counter)
- *                   load status: avg queuing time/overloaded
- * </pre>
- *
- * <p>Note：该过载保护仅适用于brief bursts，如果系统长期过载，应该扩容或改进设计.</p>
- * <ul>过载检测有2种：
- * <li>显式，例如线程池满，MQ积压超过阈值等：{@link AdmissionController#overloaded()}</li>
- * <li>隐式，更准确，但需要系统可以检测工作负载的排队时长：{@link AdmissionController#recordQueuedNs(long)}</li>
- * </ul>
- *
- * @see <a href="https://www.cs.columbia.edu/~ruigu/papers/socc18-final100.pdf">微信的过载保护</a>
- * @see <a href="https://www.usenix.org/legacy/publications/library/proceedings/usits03/tech/full_papers/welsh/welsh_html/usits.html">Adaptive Overload Control for Busy Internet Servers</a>
- * @see <a href="http://www.abelay.me/data/breakwater_osdi20.pdf">Overload Control for μs-Scale RPCs with Breakwater</a>
+ * <p>Regulate incoming requests and shed excess workload.</p>
+ * <p>Providing provision leverage effect，按照{@link WorkloadPriority}提高资源分时复用的利用率，降低整体资源成本</p>
  */
-@Slf4j
-public class AdmissionController {
-    final OverloadDetector overloadDetector;
+public interface AdmissionController {
 
-    private AdmissionController(Essence essence) {
-        overloadDetector = new OverloadDetector(essence.overloadQueuingMs,
-                essence.timeCycleNs, essence.requestCycle);
-        overloadDetector.dropRate = essence.dropRate;
-        overloadDetector.recoverRate = essence.recoverRate;
-    }
-
-    public AdmissionController() {
-        this(200);
+    /**
+     * 获取指定类型的准入控制器实例.
+     *
+     * <p>对于相同的类型，返回的是同一份实例：以类型为单位的单例.</p>
+     */
+    static AdmissionController getInstance(String kind) {
+        return FairSafeAdmissionController.getInstance(kind);
     }
 
     /**
-     * Constructor.
+     * 决定工作负荷是否准入.
      *
-     * @param overloadQueuingMs 平均排队时间超过该值则认为系统已经过载
+     * <p>只做判断，由调用者针对结果决定如何处理.</p>
+     *
+     * @param priority priority of the workload
+     * @return true if admitted, or else rejected
      */
-    public AdmissionController(long overloadQueuingMs) {
-        overloadDetector = new OverloadDetector(overloadQueuingMs);
-    }
-
-    public void setWindowSlideHook(WindowSlideHook hook) {
-        overloadDetector.hook = hook;
-    }
+    boolean admit(@NonNull WorkloadPriority priority);
 
     /**
-     * 动态调整请求排队时间阈值.
-     *
-     * <p>这是个经验值，因此实践中通常集成动态配置中心，尝试不同阈值摸索出最佳值.</p>
+     * 直接进入过载状态：显式过载反馈.
      */
-    public void adjustOverloadQueuingMs(long overloadQueuingMs) {
-        overloadDetector.overloadQueuingMs = overloadQueuingMs;
-    }
-
-    /**
-     * 根据当前的{@link AdmissionLevel}水位，决定工作负荷是否准入.
-     *
-     * @param workloadPriority 工作负荷优先级
-     * @return false if denied
-     */
-    public boolean admit(@NonNull WorkloadPriority workloadPriority) {
-        return overloadDetector.admit(workloadPriority);
-    }
+    void overloaded();
 
     /**
      * 汇报工作负荷的排队时长：隐式过载检测.
      *
      * @param queuedNs
      */
-    public void recordQueuedNs(long queuedNs) {
-        overloadDetector.addWaitingNs(queuedNs);
-    }
-
-    /**
-     * 应用已经冒烟了，直接进入过载状态：显式过载检测.
-     *
-     * <p>例如，线程池满，CPU使用率过高等</p>
-     * <p>考虑到JVM运行在容器等复杂环境，CPU是否过载由应用自行判断</p>
-     */
-    public void overloaded() {
-        overloadDetector.overloadedAtNs = System.nanoTime();
-    }
-
-    public RejectedExecutionHandler rejectedExecutionHandler() {
-        return (runnable, executor) -> {
-            // 显式过载
-            overloaded();
-        };
-    }
-
-    @Setter
-    public static class Essence {
-        private long overloadQueuingMs = 200;
-        private long timeCycleNs = SlidingWindow.DefaultTimeCycleNs;
-        private int requestCycle = SlidingWindow.DefaultRequestCycle;
-        private double dropRate = 0.05; // 5%
-        private double recoverRate = 0.01; // 1%
-
-        public AdmissionController createAdmissionController() {
-            return new AdmissionController(this);
-        }
-    }
+    void recordQueuedNs(long queuedNs);
 }
--- test/src/test/java/io/github/workload/overloading/AdmissionControllerTest.java
+++ test/src/test/java/io/github/workload/overloading/AdmissionControllerTest.java
@@ -1,56 +1,19 @@
 package io.github.workload.overloading;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.RejectedExecutionHandler;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdmissionControllerTest {
 
-    @Test
-    void basic() {
-        AdmissionController controller = new AdmissionController();
-        try {
-            controller.admit(null);
-            fail();
-        } catch (NullPointerException expected) {
-        }
-        assertTrue(controller.admit(WorkloadPriority.of(4, 6)));
-        controller.recordQueuedNs(5 * 1000_000); // 5ms
-        controller.recordQueuedNs(10 * 1000_000); // 5ms
-
-        controller.adjustOverloadQueuingMs(100);
-        RejectedExecutionHandler handler1 = controller.rejectedExecutionHandler();
-        assertNotNull(handler1);
-        RejectedExecutionHandler handler2 = controller.rejectedExecutionHandler();
-        assertNotSame(handler2, handler1);
-    }
-
-    @Test
-    void hook() {
-        AdmissionController controller = new AdmissionController(20);
-        controller.setWindowSlideHook(() -> {
-            // 例如，查询队列积压情况，来判断是否已经过载
-        });
-    }
-
-    @Test
-    void markOverloaded() throws InterruptedException {
-        AdmissionController controller = new AdmissionController();
-        OverloadDetector detector = controller.overloadDetector;
-        controller.overloaded();
-        assertTrue(detector.isOverloaded(System.nanoTime()));
-        Thread.sleep(1200); // 经过一个时间窗口
-        assertFalse(detector.isOverloaded(System.nanoTime()));
-    }
-
-    @Test
-    void essencePattern() {
-        AdmissionController.Essence essence = new AdmissionController.Essence();
-        essence.setDropRate(0.02);
-        AdmissionController controller = essence.createAdmissionController();
-        assertEquals(0.02, controller.overloadDetector.dropRate);
+    @RepeatedTest(10)
+    @Execution(ExecutionMode.CONCURRENT)
+    void getInstance() {
+        AdmissionController controller = AdmissionController.getInstance("foo");
+        assertTrue(controller instanceof FairSafeAdmissionController);
+        assertSame(controller, AdmissionController.getInstance("foo"));
+        assertNotSame(controller, AdmissionController.getInstance("bar"));
     }
 }
-
