bd50ce72dfcfaee86993b09c7896c03b4e863de9
gaopeng71 <gaopeng8@jd.com>
2023-12-27 17:54:55 +0800

    optimize: current.compareAndSet is unnecessary

--- overload-control/src/main/java/io/github/workload/window/TumblingWindow.java
+++ overload-control/src/main/java/io/github/workload/window/TumblingWindow.java
@@ -67,25 +67,21 @@ public class TumblingWindow<S extends WindowState> {
         }
 
         S nextWindow = config.createWindowState(nowNs);
-        if (current.compareAndSet(currentWindow, nextWindow)) { // TODO set
-            // 此后，采样数据都进入新窗口，currentWindow 内部状态不会再变化
-            if (log.isDebugEnabled()) {
-                // 日志的输出顺序可能与实际的窗口切换顺序不一致
-                // 窗口切换的顺序：a -> b -> c，但日志可能是：b -> c, a -> b
-                // ThreadA，完成切换：a -> b，但在日志输出前被阻塞或者调度另一个线程
-                // ThreadB，完成切换：b -> c，并且输出了日志
-                // ThreadA被调度，输出日志
-                // 如果在compareAndSet前面输出日志，那么该日志顺序与窗口切换顺序一定一致
-                currentWindow.logRollover(name, nowNs, nextWindow, config);
-            }
-
-            // 此时的 currentWindow 是该窗口的最终值
-            config.getOnRollover().accept(nowNs, currentWindow);
-            currentWindow.cleanup();
-        } else {
-            // should never happen
-            log.error("Expected to swap the window but the compareAndSet operation failed.");
+        // current.compareAndSet is unnecessary
+        current.set(nextWindow); // 此后，采样数据都进入新窗口，currentWindow 内部状态不会再变化
+        if (log.isDebugEnabled()) {
+            // 日志的输出顺序可能与实际的窗口切换顺序不一致
+            // 窗口切换的顺序：a -> b -> c，但日志可能是：b -> c, a -> b
+            // ThreadA，完成切换：a -> b，但在日志输出前被阻塞或者调度另一个线程
+            // ThreadB，完成切换：b -> c，并且输出了日志
+            // ThreadA被调度，输出日志
+            // 如果在compareAndSet前面输出日志，那么该日志顺序与窗口切换顺序一定一致
+            currentWindow.logRollover(name, nowNs, nextWindow, config);
         }
+
+        // 此时的 currentWindow 是该窗口的最终值
+        config.getOnRollover().accept(nowNs, currentWindow);
+        currentWindow.cleanup();
     }
 
 }
