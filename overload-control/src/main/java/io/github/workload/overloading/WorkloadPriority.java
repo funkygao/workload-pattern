464db0fef9330687eb2f08c1f0299db808fcc3a5
gaopeng71 <gaopeng8@jd.com>
2024-01-02 11:24:41 +0800

    FIX: P 值之前是不连续的，有空洞，反序列化时有bug. 现在是连续的

--- overload-control/src/main/java/io/github/workload/overloading/WorkloadPriority.java
+++ overload-control/src/main/java/io/github/workload/overloading/WorkloadPriority.java
@@ -39,30 +39,16 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class WorkloadPriority {
     private static final long HALF_HOUR_MS = TimeUnit.MILLISECONDS.convert(30, TimeUnit.MINUTES);
-    private static final int MAX_P = ofLowestPriority().P(); // 32639
 
-    private static final int MAX_VALUE = (1 << 7) - 1; // 127
-    private static final int LOWEST_PRIORITY = MAX_VALUE;
+    private static final int PRIORITY_BITS = 7;
+    private static final int MAX_7BIT_VALUE = (1 << PRIORITY_BITS) - 1;
 
-    private static final int U_BITS = 8;
+    @VisibleForTesting
+    static final int MAX_P = ofLowestPriority().P(); // 16383
 
     private static Map<Integer, UState> uStates = new ConcurrentHashMap<>();
 
-    /**
-     * 第一层准入机制，Business Use Case Layer Priority.
-     * <p>
-     * <p>取值范围：[0, 127]</p>
-     * <p>它反映的服务的重要性和用户体验.</p>
-     */
     private final int B;
-
-    /**
-     * 第二层准入机制, User Layer Priority.
-     * <p>
-     * <p>如果只有{@code B}，粒度太粗，会造成这样的问题：</p>
-     * <p>由于过载，某个{@code B}的请求要全部抛弃，这很快把负载降下来；随后它的请求不再抛弃，马上再次过载，如此反复jitter.</p>
-     * <p>{@code U}本质上是更细粒度的过载保护控制：partial discarding request of {@code B}.</p>
-     */
     private final int U;
 
     private WorkloadPriority(int b, int u) {
@@ -70,17 +56,9 @@ public class WorkloadPriority {
         U = u;
     }
 
-    /**
-     * 创建指定二级优先级值的工作负荷优先级.
-     *
-     * @param b user-specified 7-bit integer, lower number signifies a higher priority
-     * @param u user-specified 7-bit integer, lower number signifies a higher priority
-     * @return a new priority instance
-     * @throws IllegalArgumentException
-     */
     @VisibleForTesting
     static WorkloadPriority of(int b, int u) throws IllegalArgumentException {
-        if (b > MAX_VALUE || u > MAX_VALUE) {
+        if (b > MAX_7BIT_VALUE || u > MAX_7BIT_VALUE || b < 0 || u < 0) {
             throw new IllegalArgumentException("Out of range for B or U");
         }
 
@@ -88,45 +66,66 @@ public class WorkloadPriority {
     }
 
     /**
-     * 基于{@link #U}每半小时随机生成优先级，但{@link #B}不变.
+     * 基于{@link #U}定时随机生成优先级，但{@link #B}不变.
      *
      * @param b   {@link #B()}
      * @param uid u值特征，例如 {@code "foo".hashCode()}
-     * @return 一个u值随机的优先级
+     * @return 一个{@link #U}随机的优先级
      */
     public static WorkloadPriority ofUid(int b, int uid) {
         return timeRandomU(b, uid, HALF_HOUR_MS);
     }
 
     static WorkloadPriority ofLowestPriority() {
-        return of(LOWEST_PRIORITY, LOWEST_PRIORITY);
+        return of(MAX_7BIT_VALUE, MAX_7BIT_VALUE);
     }
 
+    /**
+     * 根据P值反序列化以重建一个新的{@link WorkloadPriority}.
+     *
+     * @param P 一个 14 位整数，其中高 7 位用来表示 `B` 值，低 7 位用来表示 `U` 值
+     * @return
+     * @throws IllegalArgumentException
+     */
     public static WorkloadPriority fromP(int P) throws IllegalArgumentException {
         if (P > MAX_P || P < 0) {
             throw new IllegalArgumentException("Invalid P");
         }
 
-        int b = (P >> U_BITS) & 0x7F;
-        int u = P & 0x7F; // 取最高的7位：0x7F(0111 1111)
+        int b = (P >> PRIORITY_BITS) & MAX_7BIT_VALUE; // 获取 B 的高 7 位
+        int u = P & MAX_7BIT_VALUE; // 获取 U 的低 7 位
         return of(b, u);
     }
 
+    /**
+     * 第一层准入机制，Business Use Case Layer Priority.
+     * <p>
+     * <p>7-bit integer, lower number signifies a higher priority: [0, 127]</p>
+     * <p>它反映的服务的重要性和用户体验.</p>
+     */
     public int B() {
         return B;
     }
 
+    /**
+     * 第二层准入机制, User Layer Priority.
+     * <p>
+     * <p>7-bit integer, lower number signifies a higher priority: [0, 127]</p>
+     * <p>如果只有{@code B}，粒度太粗，会造成这样的问题：</p>
+     * <p>由于过载，某个{@code B}的请求要全部抛弃，这很快把负载降下来；随后它的请求不再抛弃，马上再次过载，如此反复jitter.</p>
+     * <p>{@code U}本质上是更细粒度的过载保护控制：partial discarding request of {@code B}.</p>
+     */
     public int U() {
         return U;
     }
 
     /**
-     * The normalized breakwater priority value.
+     * The normalized priority value.
      *
      * <p>优先级降维：二维变一维，值越小优先级越高.</p>
      *
      * <p>它代表的是：workload delay execution tolerance</p>
-     * <p>a 14-bit integer</p>
+     * <p>a 14-bit integer，[0, 16383]</p>
      * <p>可以使用该值进行序列化传递，并通过{@link #fromP(int)}反序列化</p>
      */
     @ToString.Include
@@ -134,16 +133,17 @@ public class WorkloadPriority {
         // +--------+--------+
         // |  B(8)  |  U(8)  |
         // +--------+--------+
-        return (B << U_BITS) | U;
+        // 合并 B 和 U 为 14 位整数
+        return (B << PRIORITY_BITS) | U;
     }
 
     @VisibleForTesting
     static WorkloadPriority timeRandomU(int b, int uid, long timeWindowMs) {
-        int normalizedStableU = (uid & Integer.MAX_VALUE) % MAX_VALUE;
+        int normalizedStableU = (uid & Integer.MAX_VALUE) % MAX_7BIT_VALUE;
         long nowMs = SystemClock.ofPrecisionMs(timeWindowMs).currentTimeMillis();
         UState us = uStates.compute(normalizedStableU, (key, presentValue) -> {
             if (presentValue == null || nowMs - presentValue.createdAtMs > timeWindowMs) {
-                int randomU = ThreadLocalRandom.current().nextInt(MAX_VALUE);
+                int randomU = ThreadLocalRandom.current().nextInt(MAX_7BIT_VALUE);
                 log.trace("b:{}, create random U:{} for uid:{}", b, randomU, uid);
                 return new UState(randomU, nowMs);
             } else {
@@ -153,7 +153,7 @@ public class WorkloadPriority {
         });
 
         // GC the stale states: steal instruction cycle
-        int gcCandidateKey = ThreadLocalRandom.current().nextInt(MAX_VALUE);
+        int gcCandidateKey = ThreadLocalRandom.current().nextInt(MAX_7BIT_VALUE);
         uStates.computeIfPresent(gcCandidateKey, (key, presentValue) -> {
             if (nowMs - presentValue.createdAtMs > timeWindowMs) {
                 if (log.isDebugEnabled()) {
@@ -165,7 +165,7 @@ public class WorkloadPriority {
             }
         });
 
-        return of((b & Integer.MAX_VALUE) % MAX_VALUE, us.U);
+        return of((b & Integer.MAX_VALUE) % MAX_7BIT_VALUE, us.U);
     }
 
     @Override
