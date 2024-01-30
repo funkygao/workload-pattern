b62555e700ce8a5059e2b41f166d4f250791280f
gaopeng71 <gaopeng8@jd.com>
2024-01-30 09:13:47 +0800

    a basic AIMD

---
+++ core/src/main/java/io/github/workload/aqm/AIMD.java
@@ -0,0 +1,33 @@
+package io.github.workload.aqm;
+
+/**
+ * The additive-increase/multiplicative-decrease (AIMD) algorithm.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Additive_increase/multiplicative_decrease">wikipedia</a>
+ */
+class AIMD {
+    private volatile double windowSize; // 控制变量，比如TCP的拥塞窗口
+    private final double alpha; // 加性增长因子
+    private final double beta; // 乘性减少因子
+
+    public AIMD(double initialWindow, double alpha, double beta) {
+        this.windowSize = initialWindow;
+        this.alpha = alpha;
+        this.beta = beta;
+    }
+
+    // 当网络表现良好时调用这个方法
+    public void additiveIncrease() {
+        windowSize += alpha;
+    }
+
+    // 当网络拥塞时调用这个方法
+    public void multiplicativeDecrease() {
+        windowSize *= beta;
+    }
+
+    public double windowSize() {
+        return windowSize;
+    }
+
+}
