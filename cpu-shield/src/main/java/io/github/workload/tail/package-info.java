e5bbbe8f6b9daee6d2da226ebb97c821b76bb5c1
gaopeng71 <gaopeng8@jd.com>
2024-01-31 16:00:58 +0800

    heavy-tailed workloads

---
+++ cpu-shield/src/main/java/io/github/workload/tail/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Heavy-tail.
+ *
+ * <p>Data center workloads are often heavy-tailed and characterized by rare, expensive requests intermingled with many, simple ones.</p>
+ */
+package io.github.workload.tail;
