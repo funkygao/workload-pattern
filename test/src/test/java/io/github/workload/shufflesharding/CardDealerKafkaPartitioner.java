557f3004c82d9ff26a7babebd98e6a3fc68bd3de
gaopeng71 <gaopeng8@jd.com>
2024-02-27 18:22:29 +0800

    try partition by tenant

--- test/src/test/java/io/github/workload/shufflesharding/CardDealerKafkaPartitioner.java
+++ test/src/test/java/io/github/workload/shufflesharding/CardDealerKafkaPartitioner.java
@@ -1,18 +1,24 @@
 package io.github.workload.shufflesharding;
 
+import lombok.Data;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class CardDealerKafkaPartitioner {
 
     @RepeatedTest(1)
     @DisplayName("按照用户id分配Kafka分区")
-    void forKafkaProducePartitioner() {
+    void kafkaProducePartitionerByUID() {
         final int partitions = 32;
         final int handSize = 3;
         CardDealer dealer = CardDealer.builder()
@@ -43,10 +49,71 @@ class CardDealerKafkaPartitioner {
         System.out.println(max - min);
     }
 
+    @RepeatedTest(1)
+    void kafkaPartitionerByTenant() {
+        final int partitions = 32;
+        final int handSize = 3;
+        CardDealer dealer = CardDealer.builder()
+                .deckSize(partitions)
+                .handSize(handSize)
+                .build();
+
+        List<TenantProducer> producers = Stream.of(
+                new TenantProducer("a", 20),
+                new TenantProducer("b", 6000),
+                new TenantProducer("c", 30),
+                new TenantProducer("d", 590),
+                new TenantProducer("e", 5),
+                new TenantProducer("f", 120)
+        ).collect(Collectors.toList());
+
+        Map<Integer, List<String>> allocated = new TreeMap<>();
+        int totalWeight = producers.stream().mapToInt(TenantProducer::getWeight).sum();
+        int[] currentWeights = producers.stream().mapToInt(TenantProducer::getWeight).toArray();
+        for (int produced = 0; produced < totalWeight;) {
+            for (int i = 0; i < producers.size(); i++) {
+                if (currentWeights[i] > 0) {
+                    String tenantName = producers.get(i).name;
+
+                    final int p = partition(dealer, tenantName);
+                    List<String> messagesInPartition = allocated.get(p);
+                    if (messagesInPartition == null) {
+                        messagesInPartition = new LinkedList<>();
+                        allocated.put(p, messagesInPartition);
+                    }
+                    messagesInPartition.add(tenantName);
+
+                    currentWeights[i]--;
+                    produced++;
+                }
+            }
+        }
+        for (Integer partition : allocated.keySet()) {
+            List<String> messages = allocated.get(partition);
+            System.out.printf("%d %d\n", partition, messages.size());
+            System.out.println(messages);
+        }
+    }
+
     private int partition(CardDealer dealer, String uid) {
         int hand[] = new int[dealer.getHandSize()];
         dealer.dealIntoHand(uid, hand);
         int i = ThreadLocalRandom.current().nextInt(hand.length);
         return hand[i];
+    }
+
+    @Data
+    private static class TenantProducer {
+        String name;
+        int weight; // 权重，即该租户要发送多少条消息
+
+        TenantProducer(String name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        int getWeight() {
+            return weight;
+        }
     }
 }
