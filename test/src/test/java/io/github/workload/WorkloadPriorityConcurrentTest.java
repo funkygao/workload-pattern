53a79e7651d969482ed399079ef94977c0a8eafb
gaopeng71 <gaopeng8@jd.com>
2024-04-11 17:13:43 +0800

    n

--- test/src/test/java/io/github/workload/WorkloadPriorityConcurrentTest.java
+++ test/src/test/java/io/github/workload/WorkloadPriorityConcurrentTest.java
@@ -1,6 +1,5 @@
 package io.github.workload;
 
-import io.github.workload.WorkloadPriority;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.RepeatedTest;
