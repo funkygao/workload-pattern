b5dbb42aa2a399183f9a22db1215793bbc934609
gaopeng71 <gaopeng8@jd.com>
2024-05-08 18:06:05 +0800

    eval netflix algo

--- test/src/test/java/com/netflix/ConcurrencyEvaluationTest.java
+++ test/src/test/java/com/netflix/ConcurrencyEvaluationTest.java
@@ -1,7 +1,12 @@
 package com.netflix;
 
 import com.netflix.concurrency.limits.limit.Gradient2Limit;
+import com.netflix.concurrency.limits.limit.VegasLimit;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * https://github.com/Netflix/concurrency-limits
@@ -16,6 +21,24 @@ class ConcurrencyEvaluationTest {
         limit = Gradient2Limit.newBuilder()
                 .maxConcurrency(200)
                 .build();
+        limit.onSample(0, TimeUnit.SECONDS.toNanos(10), 5000, false);
+        assertEquals(20, limit.getLimit());
+        limit.onSample(0, TimeUnit.SECONDS.toNanos(10), 6000, false);
+        assertEquals(21, limit.getLimit());
+    }
+
+    @Test
+    void VegasLimit_demo() {
+        VegasLimit limit = VegasLimit.newBuilder()
+                .initialLimit(10000)
+                .maxConcurrency(20000)
+                .build();
+        limit.onSample(0, TimeUnit.SECONDS.toNanos(10), 5000, false);
+        assertEquals(10000, limit.getLimit());
+        limit.onSample(0, TimeUnit.SECONDS.toNanos(10), 6000, false);
+        assertEquals(10024, limit.getLimit());
+        limit.onSample(0, TimeUnit.SECONDS.toNanos(20), 3000, true);
+        assertEquals(10020, limit.getLimit());
     }
 
 }
