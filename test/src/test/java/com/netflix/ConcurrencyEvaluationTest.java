e4cf762ac37eb978a154a5d9ba4e2be4ead1c982
gaopeng71 <gaopeng8@jd.com>
2024-05-10 09:34:36 +0800

    比较netflix concurrency预测算法

--- test/src/test/java/com/netflix/ConcurrencyEvaluationTest.java
+++ test/src/test/java/com/netflix/ConcurrencyEvaluationTest.java
@@ -1,6 +1,7 @@
 package com.netflix;
 
 import com.netflix.concurrency.limits.limit.Gradient2Limit;
+import com.netflix.concurrency.limits.limit.GradientLimit;
 import com.netflix.concurrency.limits.limit.VegasLimit;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @see <a href="https://netflixtechblog.medium.com/performance-under-load-3e6fa9a60581">Performance Under Load</a>
  */
 class ConcurrencyEvaluationTest {
+
+    // https://github.com/apache/brpc/blob/master/docs/cn/auto_concurrency_limiter.md
+    void explain_limit_algo() {
+        // Gradient限流算法是计算无负载时的RTT与当前RTT的比值来判断是否出现请求排队的情况，类似 CoDel
+        // gradient = RTT_no_load / RTT_actual，梯度小于1则说明开始排队了，需要降低limit
+        // newLimit = currentLimit * gradient + queueSize, queueSize是当前limit的sqrt
+        GradientLimit.newDefault().onSample(0, 23, 100, false);
+
+        // GradientLimit在heavy request场景下不好，容易出现过度保护问题
+        // Gradient2Limit缓解了该问题：LongRTT
+        Gradient2Limit.newDefault();
+
+        // VegasLimit直接使用等待队列的大小queueSize进行判断是否限流
+        // queueSize = limit * (1 - RTT_no_load / RTT_actual)
+        // VegasLimit也存在过度保护问题
+        VegasLimit.newDefault();
+    }
 
     @Test
     void Gradient2Limit_demo() {
