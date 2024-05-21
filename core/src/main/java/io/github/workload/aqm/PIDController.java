34ba3e2a902ebc729c39f0f5e038608b89ec7fb7
Funky Gao <funky.gao@gmail.com>
2024-05-21 20:44:48 +0800

    doc

--- core/src/main/java/io/github/workload/aqm/PIDController.java
+++ core/src/main/java/io/github/workload/aqm/PIDController.java
@@ -6,12 +6,10 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * PID控制器，用于实现比例-积分-微分（PID）控制。
+ * PID控制器，用于实现比例-积分-微分（PID）控制.
  * <p>
- * PID控制器是一种常见的反馈控制器，广泛应用于工业控制系统中。
- * 它通过计算设定点（期望值）与实际测量值之间的偏差，使用比例（P）、积分（I）和微分（D）三个参数
- * 来调整控制输入，以达到快速响应、减少偏差和稳定系统的目的。
- * </p>
+ * <p>只要被控制量与输入量之间存在单调关系的反馈场景基本上都能用，以达到快速响应、减少偏差和稳定系统的目的.</p>
+ * <p>物联网应用，例如：云计算，PID用于动态优化资源分配，以应对不断变化的负载和用户需求.</p>
  */
 @ThreadSafe
 public class PIDController {
@@ -41,12 +39,9 @@ public class PIDController {
      * 根据当前的误差和时间计算PID控制器的输出。
      * 输出值的范围和符号取决于误差大小、PID增益参数以及误差随时间的变化。
      *
-     * @param error 当前的误差，表示目标状态和当前状态之间的差距。
-     * @param nowNs 当前时间（纳秒），用于计算时间间隔（dt）。
-     * @return 控制器输出，该值指示为了减少误差需要对系统进行的调整。
-     * 输出值的大小表示调整的幅度，符号（正或负）表示调整的方向。
-     * 例如，在一个加热系统中，正值可能意味着需要增加加热量，而负值可能意味着需要减少加热量。
-     * 输出值的范围没有固定限制，取决于PID参数和输入误差的大小。
+     * @param error 偏差，表示目标状态和当前状态之间的差距
+     * @param nowNs 当前时间（纳秒），用于计算时间间隔（dt）
+     * @return 控制器输出，用于调整控制量
      */
     public double getOutput(double error, long nowNs) {
         integral.updateAndGet(value -> value + error);
@@ -67,17 +62,13 @@ public class PIDController {
             lastError = error; // 更新上一次的偏差
         }
 
-        // 比例控制P，是最直接的控制方式，它根据系统当前的误差来调整输出。误差越大，控制器的调整幅度也越大
+        // 比例控制P，快速响应，但也容易导致用力过猛
         final double P = kp * error;
 
-        // 积分控制I，考虑了误差随时间的累积效应。
-        // 如果系统存在稳态误差，即使P已经达到最大或最小输出，系统仍然无法达到目标值
-        // 积分控制通过累积误差来调整输出，以消除稳态误差
-
+        // 积分控制I，考虑了偏差随时间的累积效应，消除稳态误差，确保长期稳定
         final double I = ki * integral.get();
 
-        //微分控制D，关注误差的变化率，即系统输出的变化趋势
-        // 通过预测误差的未来变化，微分控制可以提高系统的响应速度，减少超调和振荡
+        // 微分控制D，预测偏差趋势，对快速响应中和，避免过度响应
         final double D = kd * derivative;
 
         return P + I + D;
