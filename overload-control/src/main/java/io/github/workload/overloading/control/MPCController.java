a6b7a9bb035e30cc7eef6fa78acb14845bc75251
gaopeng71 <gaopeng8@jd.com>
2024-05-28 15:35:48 +0800

    WIP MPC

--- overload-control/src/main/java/io/github/workload/overloading/control/MPCController.java
+++ overload-control/src/main/java/io/github/workload/overloading/control/MPCController.java
@@ -1,8 +1,39 @@
 package io.github.workload.overloading.control;
 
+import io.github.workload.annotations.NotThreadSafe;
+
+import java.util.ArrayDeque;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Model Predictive Control.
  */
+@NotThreadSafe
 class MPCController {
+    private final ArrayDeque<Double> predictions = new ArrayDeque<>();
+    private double shedProbability = 1d;
 
+    public MPCController(int queueLength) {
+        for (int i = 0; i < queueLength; i++) {
+            predictions.addLast(1d);
+        }
+    }
+
+    // 更新预测模型
+    public MPCController updateModel(double data) {
+        predictions.pollFirst(); // 移除旧的预测
+        predictions.addLast(data); // 添加新的预测
+
+        final double avg = predictions.stream().mapToDouble(i -> i).average().orElse(0.0);
+        if (avg > 2) {
+            shedProbability = Math.min(1.0, shedProbability + 0.1);
+        } else {
+            shedProbability = Math.max(0.0, shedProbability - 0.1);
+        }
+        return this;
+    }
+
+    public boolean shouldShed() {
+        return ThreadLocalRandom.current().nextDouble() < shedProbability;
+    }
 }
---
+++ test/src/test/java/io/github/workload/overloading/control/MPCControllerTest.java
@@ -0,0 +1,16 @@
+package io.github.workload.overloading.control;
+
+import io.github.workload.BaseTest;
+import org.junit.jupiter.api.Test;
+
+class MPCControllerTest extends BaseTest {
+    MPCController controller = new MPCController(4);
+
+    @Test
+    void basic() {
+        double[] shedRatioErrors = new double[]{0.1, 0.02, 0.5, 0.6, 0.8, 0.4, 1.2};
+        for (double err : shedRatioErrors) {
+            log.info("err:{}, shouldShed:{}", err, controller.updateModel(err).shouldShed());
+        }
+    }
+}
