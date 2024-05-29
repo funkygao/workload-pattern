e7c25ea818c82fecf725798ef5fc2df87a56eaaf
gaopeng71 <gaopeng8@jd.com>
2024-05-29 11:10:17 +0800

    stress test

--- test/src/test/java/io/github/workload/overloading/WatermarkHistoryTest.java
+++ test/src/test/java/io/github/workload/overloading/WatermarkHistoryTest.java
@@ -11,14 +11,14 @@ class WatermarkHistoryTest extends BaseTest {
 
     @Test
     void basic() {
-        // 10
-        double[] shedRatios = new double[]{0.1, 0.15, 0.08, 0, 0, 0.01, 0.21, 0.3, 0.31, 0.23};
-        int[] watermarkPs = new int[]{4590, 3456, 8988, 8763, 9999, 345, 2345, 5698, 4521, 321};
-        for (int i = 0; i < shedRatios.length; i++) {
-            WorkloadPriority watermark = WorkloadPriority.fromP(watermarkPs[i]);
-            history.addHistory(shedRatios[i], watermark);
+        for (int i = 0; i < 1 << 20; i++) {
+            double[] shedRatios = new double[]{0.1, 0.15, 0.08, 0, 0, 0.01, 0.21, 0.3, 0.31, 0.23};
+            int[] watermarkPs = new int[]{4590, 3456, 8988, 8763, 9999, 345, 2345, 5698, 4521, 321};
+            for (int j = 0; j < shedRatios.length; j++) {
+                WorkloadPriority watermark = WorkloadPriority.fromP(watermarkPs[j]);
+                history.addHistory(shedRatios[j], watermark);
+            }
+            assertEquals(2345, history.lastWatermark().P());
         }
-        log.info("avg:{}, {}", history.averageShedRatio(), history.lastWatermark());
-        assertEquals(2345, history.lastWatermark().P());
     }
 }
