05a7cd6fa55dd15cd20bcc9e1a1bb9d449d44628
gaopeng71 <gaopeng8@jd.com>
2024-05-31 13:56:14 +0800

    explain the formula

--- test/src/test/java/com/netflix/ConcurrencyEvaluationTest.java
+++ test/src/test/java/com/netflix/ConcurrencyEvaluationTest.java
@@ -59,4 +59,23 @@ class ConcurrencyEvaluationTest {
         assertEquals(10020, limit.getLimit());
     }
 
+    // (maxPass * windows * minRttMs) / 1000
+    //
+    // 100ms为1个窗口，maxPass 是过去50个窗口(5s)里，admitted 最多的那个窗口里的 admitted value
+    // minRttMs是以窗口为单位的最小平均请求耗时
+    // windows 是1秒有多少个窗口，这里为10
+    // 例如：maxPass=500, minRttMs=50，则当前系统容量(并发数)：250
+    //
+    // maxPass * window：每秒成功处理的请求数(在所有窗口内)
+    // minRttMs / 1000：把 minRttMs 时间单位转换为秒，以便能与请求数进行计算
+    //                  把这个理解为时间跨度：一个请求的时间跨度为 minRttMs
+    // 公式整体内涵：在最小rtt时间内，系统能够并发处理的请求数量
+    // maxPass * window，是QPS，是把该window平铺到1秒上，那么在任何一个 minRttMs 的时间区间内，请求的数量可以表示为：
+    // QPS * minRttMs / 1000
+    //
+    // 总结：该公式计算的是在最小往返时间内，系统能够并发处理的请求数量。通过将每秒的请求数均匀分布在时间线上，并考虑请求的最小往返时间，可以估算出系统的并发处理能力。
+    private void calculateMaxConcurrency() {
+
+    }
+
 }
