211bc39f8cdb37ed6f6603e3c6a1b1f4f9ffa5a8
gaopeng71 <gaopeng8@jd.com>
2024-06-03 10:31:53 +0800

    fix: 线性插值计算概率

--- overload-control/src/main/java/io/github/workload/overloading/ShedStochastic.java
+++ overload-control/src/main/java/io/github/workload/overloading/ShedStochastic.java
@@ -2,31 +2,62 @@ package io.github.workload.overloading;
 
 import io.github.workload.WorkloadPriority;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * 概率式削减：一种简单且有效解决请求优先级分布不均的方法.
+ * 概率式削减：对已经要削减的工作负荷，进行基于概率的二次确认.
  *
+ * <p>一种简单且有效解决请求优先级分布不均的方法</p>
  * <p>对于低优先级请求，不是完全拒绝，而是按一定的概率拒绝.</p>
  * <p>这种方法可以确保每个优先级级别的请求都能得到一定处理，同时又可以限制低优先级请求对系统的影响.</p>
  */
+@Slf4j
 class ShedStochastic {
-    private final Map<Integer, Double> shedProbabilities = new HashMap<>();
+    private final TreeMap<Integer, Double> shedProbabilities;
+
+    private ShedStochastic(TreeMap<Integer, Double> shedProbabilities) {
+        this.shedProbabilities = shedProbabilities;
+    }
 
     static ShedStochastic newDefault() {
-        ShedStochastic stochastic = new ShedStochastic();
-        stochastic.shedProbabilities.put(WorkloadPriority.B_CRITICAL_PLUS, 0.8);
-        stochastic.shedProbabilities.put(WorkloadPriority.B_CRITICAL, 0.85);
-        stochastic.shedProbabilities.put(WorkloadPriority.B_SHEDDABLE_PLUS, 0.95);
-        stochastic.shedProbabilities.put(WorkloadPriority.B_SHEDDABLE, 1d);
-        return stochastic;
+        TreeMap<Integer, Double> probabilities = new TreeMap<>();
+        probabilities.put(WorkloadPriority.B_CRITICAL_PLUS, 0.85);
+        probabilities.put(WorkloadPriority.B_CRITICAL, 0.91);
+        probabilities.put(WorkloadPriority.B_SHEDDABLE_PLUS, 0.95);
+        probabilities.put(WorkloadPriority.B_SHEDDABLE, 1d);
+        return new ShedStochastic(probabilities);
     }
 
     boolean shouldShed(@NonNull WorkloadPriority priority) {
-        final double prob = shedProbabilities.getOrDefault(priority.B(), 1d);
+        final int B = priority.B();
+        Double prob = shedProbabilities.get(B);
+        if (prob != null) {
+            return ThreadLocalRandom.current().nextDouble() < prob;
+        }
+
+        // 查找最近的上下界
+        Map.Entry<Integer, Double> lowerEntry = shedProbabilities.floorEntry(B);
+        Map.Entry<Integer, Double> higherEntry = shedProbabilities.ceilingEntry(B);
+        if (lowerEntry == null) {
+            // B < B_CRITICAL_PLUS
+            prob = higherEntry.getValue();
+        } else if (higherEntry == null) {
+            // B > B_SHEDDABLE
+            prob = lowerEntry.getValue();
+        } else {
+            // 线性插值计算概率
+            final int lowerKey = lowerEntry.getKey();
+            final int higherKey = higherEntry.getKey();
+            final double lowerValue = lowerEntry.getValue();
+            final double higherValue = higherEntry.getValue();
+            prob = lowerValue + (higherValue - lowerValue) * (B - lowerKey) / (double) (higherKey - lowerKey);
+        }
+
+        log.trace("B:{} prob:{}", B, prob);
         return ThreadLocalRandom.current().nextDouble() < prob;
     }
 
--- test/src/test/java/io/github/workload/overloading/ShedStochasticTest.java
+++ test/src/test/java/io/github/workload/overloading/ShedStochasticTest.java
@@ -2,6 +2,8 @@ package io.github.workload.overloading;
 
 import io.github.workload.BaseTest;
 import io.github.workload.WorkloadPriority;
+import org.apache.logging.log4j.Level;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -23,11 +25,39 @@ class ShedStochasticTest extends BaseTest {
             }
             log.info("B:{} stochastic:{}/{}", priority.B(), shedded, N);
         }
+    }
 
-        WorkloadPriority priority = WorkloadPriority.ofPeriodicRandomFromUID(8, 3);
-        for (int i = 0; i < 10; i++) {
-            assertTrue(stochastic.shouldShed(priority));
+    @Test
+    void linearInterpolation() {
+        setLogLevel(Level.TRACE);
+        ShedStochastic stochastic = ShedStochastic.newDefault();
+        for (int B : new int[]{WorkloadPriority.B_CRITICAL_PLUS - 2, WorkloadPriority.B_CRITICAL - 2, WorkloadPriority.B_SHEDDABLE_PLUS - 2, WorkloadPriority.B_SHEDDABLE - 2, WorkloadPriority.B_SHEDDABLE + 2}) {
+            WorkloadPriority priority = WorkloadPriority.ofPeriodicRandomFromUID(B, 1);
+            log.info("B:{} {}", priority.B(), stochastic.shouldShed(priority));
         }
+        setLogLevel(Level.DEBUG);
+    }
+
+    @Test
+    @Disabled
+    void benchmark() {
+        ShedStochastic stochastic = ShedStochastic.newDefault();
+        WorkloadPriority priority = WorkloadPriority.ofPeriodicRandomFromUID(WorkloadPriority.B_CRITICAL, 5);
+        long t0 = System.nanoTime();
+        int N = 1 << 30;
+        // 空循环用于基准测试，同时引入不可预测的计算，确保JVM不会将空循环优化掉
+        long dummy = 0;
+        for (int i = 0; i < N; i++) {
+            dummy += i;
+        }
+        double baseOps = (double) (System.nanoTime() - t0) / N;
+        t0 = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            stochastic.shouldShed(priority);
+        }
+        final double ops = (double) (System.nanoTime() - t0) / N - baseOps;
+        log.info("{} ops/ns, base:{}", (double) (System.nanoTime() - t0) / N - baseOps, baseOps);
+        assertTrue(ops < 10);
     }
 
 }
